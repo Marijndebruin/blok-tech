f2832fb98212ea87f79ba34575124dbfe05a7a39
marijndebruin <marijndebruinn@gmail.com>
2022-03-27 21:24:13 +0200

    Uodate

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// dotenv hieroor maak je een .env bestand en sla hier je gegevens voor de database in op.
+require('dotenv').config()
+console.log(process.env) // remove this after you've confirmed it working
+
+// Database dingen
+const {
+  MongoClient
+} = require('mongodb');
+const {
+  ObjectId
+} = require('mongodb');
+
 // View engines ik heb gekozen voor EJS.
 app.set('view engine', 'ejs');
 
@@ -24,6 +36,25 @@ app.get('/resultaat', (req, res) => {
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
+  connectDB().then(() => console.log("We have a connection to Mongo!"));
+
 })
 
 app.use(express.static('public'))
+
+
+// Database
+// connect
+async function connectDB() {
+  const uri = process.env.DB_URI;
+  const client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  });
+  try {
+      await client.connect();
+      db = client.db(process.env.DB_NAME);
+  } catch (error) {
+      throw error;
+  }
+}
